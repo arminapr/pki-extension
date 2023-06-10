5f9c3de45f5b0db9c5777a74e82627d8e43907ae
arminapr <115113212+arminapr@users.noreply.github.com>
2023-06-09 20:04:59 -0400

    dummy values + some functions

---
+++ extension-trial/popup/change.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var nonSens = document.getElementById("nonSensitive");
+    var sens = document.getElementById("sensitive");
+
+    // call the functions to mark the website as either sensitive or not sensitive
+    nonSens.addEventListener("click", () => {
+        markWebsiteNonSensitive()});
+    sens.addEventListener("click", () => {
+        markWebsiteSensitive()});
+
+    function markWebsiteNonSensitive() {
+        // write this function
+        var imageSrc = '../icons/sample-pic.png'; // Replace with the path to your image
+        var image = document.createElement('img');
+        image.src = imageSrc;
+        nonSens.appendChild(image);
+    }
+
+    function markWebsiteSensitive() {
+        // write this function
+    }
+})
+
+// we can't set this in checkChange because it would update every time
+var prevCA; 
+if (!prevCA) {
+    prevCA = "Distrust"; // dummy value
+}
+
+
+function checkChange(url) {
+    const currCA = "Entrust"; // dummy value
+    // check if the current CA is the same as the previous one
+    if (currCA != prevCA) {
+        var shouldTrust = window.confirm('The digital certificate has changed. Do you still trust this website?');
+
+        if (shouldTrust) {
+            prevCA = currCA;
+        } else {
+            markWebsiteNotSecure();
+        }
+    }
+}
