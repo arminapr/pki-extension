a782930c2355c75a467d2e6afbd1e87a431f3bb9
arminapr <115113212+arminapr@users.noreply.github.com>
2023-06-14 11:08:03 -0400

    trying the block version

--- extension-trial/background_scripts/background.js
+++ extension-trial/background_scripts/background.js
@@ -1,1 +1,14 @@
 // this will handle the case if the CA information has changed
+var blocked = true;
+
+browser.browserAction.onClicked.addListener(reloadPage);
+
+function reloadPage() {
+  if (blocked) {
+    blocked = false;
+    browser.tabs.reload();
+  } else {
+    blocked = true;
+    browser.tabs.update({ url: "about:blank" });
+  }
+}
