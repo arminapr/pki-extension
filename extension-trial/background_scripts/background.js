e35c379b7850926661fa7e94fdcf2076c4000a74
arminapr <115113212+arminapr@users.noreply.github.com>
2023-06-14 11:41:12 -0400

    trying to block the page

--- extension-trial/background_scripts/background.js
+++ extension-trial/background_scripts/background.js
@@ -1,14 +1,2 @@
-// this will handle the case if the CA information has changed
-var blocked = true;
-
-browser.browserAction.onClicked.addListener(reloadPage);
-
-function reloadPage() {
-  if (blocked) {
-    blocked = false;
-    browser.tabs.reload();
-  } else {
-    blocked = true;
-    browser.tabs.update({ url: "about:blank" });
-  }
-}
+/* write a function that would take a click on the extension and unblock
+    the page */
