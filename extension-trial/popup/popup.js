9e9cdee6d050764d908257a26a0bc66b14a0b021
arminapr <115113212+arminapr@users.noreply.github.com>
2023-06-16 20:18:11 -0400

    implementing more functionality within the main branch

--- extension-trial/popup/popup.js
+++ extension-trial/popup/popup.js
@@ -1,29 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-    var notMarked = document.getElementById("notMarked");
-    var safe = document.getElementById("safe");
-    var misMarked = document.getElementById("misMarked");
-    var misClicked = document.getElementById("misClicked");
-    var markedSame = document.getElementById("markedSameCert");
-    var markedDiff = document.getElementById("markedDiffCert");
-    var nonSens = document.getElementById("markedNonSensitive");
-    var faviconImage = document.getElementById('faviconImage'); //Favicon (Logo)
-    var websiteUrlElement = document.getElementById('websiteUrl'); //URL
+    const notMarked = document.getElementById("notMarked");
+    const safe = document.getElementById("safe");
+    const misMarked = document.getElementById("misMarked");
+    const misClicked = document.getElementById("misClicked");
+    const trustText = document.getElementById("trustText");
+    const untrustText = document.getElementById("untrustText");
+    const markedSame = document.getElementById("markedSameCert");
+    const markedDiff = document.getElementById("markedDiffCert");
+    const nonSens = document.getElementById("markedNonSensitive");
+    const faviconImage = document.getElementById("faviconImage"); //Favicon (Logo)
+    const websiteUrlElement = document.getElementById("websiteUrl"); //URL
+    // retreieves the list of safe and unsafe websites from the local storage
+    const safeWebsites = localStorage.getItem("safeList");
+    const unsafeWebsites = localStorage.getItem("unsafeList");
+    // if they exist, retrieve them, if not make an empty array
+    const safeList = safeWebsites ? JSON.parse(safeWebsites) : [];
+    const unsafeList = unsafeWebsites ? JSON.parse(unsafeWebsites) : [];
 
-    var safeWebsites = localStorage.getItem("safeList");
-    if (safeWebsites) {
-        var safeList = JSON.parse(storedList);
-    } else {
-        var safeList = [];
+    function applyTo(func) {
+        browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            const currentTab = tabs[0];
+            const url = currentTab.url;
+            func(url);
+        });
     }
-    
-    /*
-     * Tab Info, Favicon, and URL
-     */
-    browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        var currentTab = tabs[0];
-        var url = currentTab.url;
+
+    // add a domain to the safe list
+    function addSafeWebsite(url) {
+        safeList.push(url);
+        notMarked.style.display="none";
+        trustText.style.display="block";
+    }
+
+    // add a domain to the unsafe list
+    function addUnsafeWebsite(url) {
+        unsafeList.push(url);
+        notMarked.style.display="none";
+        untrustText.style.display="block";
+    }
+
+    // update the website information on the extension
+    function updateInfo(tab) {
+        const url = tab.url;
         if (safeList.includes(url)) {
-            notMarked.style.display="none";
+            notMarked.style.display = "none";
             // TODO: add a conditional comparing the details of the certificates and if they are the same
             /*
              * if (prev certificate component != current) {
@@ -36,29 +56,19 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         //Display the website URL
         websiteUrlElement.textContent = url;
+        faviconImage.src = tab.favIconUrl;
+    }
 
-        // Get the favicon URL and set the HTML img content
-        var faviconUrl = currentTab.favIconUrl;
-        faviconImage.src = faviconUrl;
-
-        // if they click on the safe button, add the website to a list
-        safe.addEventListener("click", () => {
-            addSafeWebsite();
-        })
-
-        function addSafeWebsite() {
-            safeList.push(url);
-        }
+    // get the information on the extension
+    browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        updateInfo(tabs[0]);
     });
 
-    
-    
-    
 
     // Receive message from background.js for CA Info and update html
     browser.runtime.onMessage.addListener(
         (request, sender, sendResponse) => {
-            if (request.rootCA){ // checking if root CA exists in the requesr
+            if (request.rootCA) { // checking if root CA exists in the requesr
                 const rootCAInfoElement = document.getElementById("rootCAInfo");
                 rootCAInfoElement.textContent = request.rootCA;
             }
@@ -66,50 +76,27 @@ document.addEventListener("DOMContentLoaded", () => {
     );
 
 
-    // Call the functions to mark the website as either sensitive or not sensitive
-    nonSens.addEventListener("click", () => {
-        markWebsiteNonSensitive()});
-    sens.addEventListener("click", () => {
-        markWebsiteSensitive()});
-    nonSecure.addEventListener("click", () => {
-        markWebsiteNonSecure()}); // have to write this function
-    secure.addEventListener("click", () => {
-        markWebsiteSecure()}); // have to write this function
-
-    // this is a dummy function right now
-    function markWebsiteNonSensitive() {
-        // write this function
-        var imageSrc = '../icons/sample-pic.png';
-        var image = document.createElement('img');
-        image.src = imageSrc;
-        nonSens.appendChild(image);
-    }
-
-    // changes the question and asks whether the website is secure
-    function markWebsiteSensitive() {
-        sensitiveQ.style.display = "none";
-        secureQ.style.display = "block";
-    }
-
-    // changes the question to ask why the website is not secure
-    function markWebsiteNonSecure() {
-        secureQ.style.display = "none";
-        trustQ.style.display = "block";
-    }
-
-    function markWebsiteSecure() {
-        secureQ.style.display = "none";
-        secureText.style.display = "block";
-    }
+    // if they click on the safe button, add the website to a list
+    safe.addEventListener("click", () => {
+        applyTo(addSafeWebsite);
+    });
+    misMarked.addEventListener("click", () => {
+        applyTo(addUnsafeWebsite);
+    });
+    misClicked.addEventListener("click", () => {
+        setTimeout(() => {
+            window.close(); // I don't think this works
+        }, 100)
+    });
 })
 
 // we can't set this in checkChange because it would update every time
-var prevCA; 
+var prevCA;
 if (!prevCA) {
     prevCA = "Distrust"; // dummy value
 }
 
-function checkChange(url) {
+/* function checkChange(url) {
     const currCA = "Entrust"; // dummy value
     // check if the current CA is the same as the previous one
     if (currCA != prevCA) {
@@ -118,7 +105,7 @@ function checkChange(url) {
         if (shouldTrust) {
             prevCA = currCA;
         } else {
-            markWebsiteNotSecure();
+            // markWebsiteNotSecure();
         }
     }
-}
+} */
