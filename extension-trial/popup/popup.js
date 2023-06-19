39b1c4c913e054a94fccc18c9234137a976c89ef
arminapr <115113212+arminapr@users.noreply.github.com>
2023-06-19 16:13:33 -0400

    indentation fix

--- extension-trial/popup/popup.js
+++ extension-trial/popup/popup.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     var faviconImage = document.getElementById('faviconImage'); //Favicon (Logo)
     var websiteUrlElement = document.getElementById('websiteUrl'); //URL
     // extracting the list of safe and unsafe websites from local storage
-    const safeWebsites = localStorage.getItem("safeList"); 
+    const safeWebsites = localStorage.getItem("safeList");
     const unsafeWebsites = localStorage.getItem("unsafeList");
     // if they exist in local storage, retrieve them; otherwise, make a new list
     const safeList = safeWebsites ? JSON.parse(safeWebsites) : [];
@@ -27,34 +27,33 @@ document.addEventListener("DOMContentLoaded", () => {
         // Receive message from background.js for CA Info and update html
         browser.runtime.onMessage.addListener(
             (request, sender, sendResponse) => {
-                if (request.rootCA){ // checking if root CA exists in the request
-                caInfo = request.rootCA;
-                const rootCAInfoElement = document.getElementById("rootCAInfo");
-                rootCAInfoElement.textContent = caInfo;
-                checkCA(url);
+                if (request.rootCA) { // checking if root CA exists in the request
+                    caInfo = request.rootCA;
+                    const rootCAInfoElement = document.getElementById("rootCAInfo");
+                    rootCAInfoElement.textContent = caInfo;
+                    checkCA(url);
+                }
             }
-        }
-    );
+        );
 
-    // if they click on the safe button, add the website to a list
-    safe.addEventListener("click", function() {
-        addToSensitive(url);
-        notMarked.style.display = "none";
-        markedSame.style.display = "block";
-    });
-    misMarked.addEventListener("click", function() {
-        addToBlock(url);
-    });
-    misClicked.addEventListener("click", () => {
-        window.close();
-    });
+        // if they click on the safe button, add the website to a list
+        safe.addEventListener("click", function () {
+            addToSensitive(url);
+            notMarked.style.display = "none";
+            markedSame.style.display = "block";
+        });
+        misMarked.addEventListener("click", function () {
+            addToBlock(url);
+        });
+        misClicked.addEventListener("click", () => {
+            window.close();
+        });
 
     });
 
     /**
     // update the website information on the extension
     function updateInfo(tab) {
-        const url = tab.url;
         /**
         if (safeList.includes(url)) {
             notMarked.style.display = "none";
@@ -68,87 +67,82 @@ document.addEventListener("DOMContentLoaded", () => {
              * }
              
         }
-        
-
-        //Display the website URL and favicon
-        websiteUrlElement.textContent = url;
-        faviconImage.src = tab.favIconUrl;
     }
     */
 
-// Retrieve the stored information for the current website
-function retrieveStoredInfo(url){
-    return new Promise((resolve) => {
-        browser.storage.local.get(url).then((result) => {
-            if (result[url]) {
-                const [storedCAInfo, isSensitive] = result[url];
-                resolve({ storedCAInfo, isSensitive});
-            } else {
-                resolve(null);
-            }
+    // Retrieve the stored information for the current website
+    function retrieveStoredInfo(url) {
+        return new Promise((resolve) => {
+            browser.storage.local.get(url).then((result) => {
+                if (result[url]) {
+                    const [storedCAInfo, isSensitive] = result[url];
+                    resolve({ storedCAInfo, isSensitive });
+                } else {
+                    resolve(null);
+                }
+            });
         });
-    });
-}
-
-// Update the popup based on the stored information and CA info comparison
-function updatePopup(url, storedCAInfo, isSensitive){
-    const notice = document.getElementById("notice");
-    
-    if (isSensitive){
-        notice.textContent = "This website has been marked sensitive. ";
-    } else {
-        notice.textContent = "This website has NOT been marked sensitive. ";
     }
 
-    //Compare CA Info
-    if (storedCAInfo && storedCAInfo !== caInfo) {
-        notice.textContent += "CA Info Changed";
-    } else {
-        notice.textContent += "CA Info NOT Changed";
-    }
-}
+    // Update the popup based on the stored information and CA info comparison
+    function updatePopup(url, storedCAInfo, isSensitive) {
+        const notice = document.getElementById("notice");
 
-// Get the current tab URL, retrieve stored information, and update the popup
-function tabUpdate(){
-    const queryInfo = { active: true, currentWindow: true};
-
-    browser.tabs.query(queryInfo).then((tabs) => {
-        const currentTab = tabs[0];
-        const currentUrl = currentTab.url;
-
-        retrieveStoredInfo(currentUrl).then((storedInfo) => {
-            if (storedInfo) {
-                const { storedCAInfo, isSensitive } = storedInfo;
-                updatePopup(currentUrl, storedCAInfo, isSensitive);
-            } else {
-                updatePopup(currentUrl, null, false);
-            }
-        });
-    });
-}
-
-browser.tabs.onActivated.addListener(tabUpdate); //event listener for tab change
-
-tabUpdate(); //initial function call on popup load
-
-//save caInfo and mark sensitive
-function addToSensitive(url){
-    browser.storage.local.set({ [url]: [caInfo, true] });
-}
-
-//save caInfo and block
-function addToBlock(url){
-    browser.storage.local.set({ [url]: [caInfo, false] });
-}
-
-//Check if CA Info has changed 
-function checkCA(url){
-    retrieveStoredInfo(url).then((storedInfo) => {
-        if (storedInfo && storedInfo.storedCAInfo !== caInfo) {
-            browser.storage.local.set({ [url]: [caInfo, storedInfo.isSensitive]});
+        if (isSensitive) {
+            notice.textContent = "This website has been marked sensitive. ";
+        } else {
+            notice.textContent = "This website has NOT been marked sensitive. ";
         }
-    });
-}
+
+        //Compare CA Info
+        if (storedCAInfo && storedCAInfo !== caInfo) {
+            notice.textContent += "CA Info Changed";
+        } else {
+            notice.textContent += "CA Info NOT Changed";
+        }
+    }
+
+    // Get the current tab URL, retrieve stored information, and update the popup
+    function tabUpdate() {
+        const queryInfo = { active: true, currentWindow: true };
+
+        browser.tabs.query(queryInfo).then((tabs) => {
+            const currentTab = tabs[0];
+            const currentUrl = currentTab.url;
+
+            retrieveStoredInfo(currentUrl).then((storedInfo) => {
+                if (storedInfo) {
+                    const { storedCAInfo, isSensitive } = storedInfo;
+                    updatePopup(currentUrl, storedCAInfo, isSensitive);
+                } else {
+                    updatePopup(currentUrl, null, false);
+                }
+            });
+        });
+    }
+
+    browser.tabs.onActivated.addListener(tabUpdate); //event listener for tab change
+
+    tabUpdate(); //initial function call on popup load
+
+    //save caInfo and mark sensitive
+    function addToSensitive(url) {
+        browser.storage.local.set({ [url]: [caInfo, true] });
+    }
+
+    //save caInfo and block
+    function addToBlock(url) {
+        browser.storage.local.set({ [url]: [caInfo, false] });
+    }
+
+    //Check if CA Info has changed 
+    function checkCA(url) {
+        retrieveStoredInfo(url).then((storedInfo) => {
+            if (storedInfo && storedInfo.storedCAInfo !== caInfo) {
+                browser.storage.local.set({ [url]: [caInfo, storedInfo.isSensitive] });
+            }
+        });
+    }
 });
 
 
