f303e75124aff89f65c01cf20f1124da1080d887
akhargha <kharghariaanupam07@gmail.com>
2023-06-19 04:27:39 -0400

    Add global CAInfo to popup.js

--- extension-trial/popup/popup.js
+++ extension-trial/popup/popup.js
@@ -48,9 +48,10 @@ document.addEventListener("DOMContentLoaded", () => {
     // Receive message from background.js for CA Info and update html
     browser.runtime.onMessage.addListener(
         (request, sender, sendResponse) => {
-            if (request.rootCA){ // checking if root CA exists in the requesr
+            if (request.rootCA){ // checking if root CA exists in the request
+                caInfo = request.rootCA;
                 const rootCAInfoElement = document.getElementById("rootCAInfo");
-                rootCAInfoElement.textContent = request.rootCA;
+                rootCAInfoElement.textContent = caInfo;
             }
         }
     );
