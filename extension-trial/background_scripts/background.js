29510b4333219168ac1decf26e156d344ffe2e34
arminapr <115113212+arminapr@users.noreply.github.com>
2023-06-26 14:01:15 -0400

    testing adding a new event listener

--- extension-trial/background_scripts/background.js
+++ extension-trial/background_scripts/background.js
@@ -1,7 +1,13 @@
 ("use strict");
 /* write a function that would take a click on the extension and unblock
     the page */
-
+browser.runtime.onMessage.addListener((message) => {
+    if (message.data) {
+        if (message.data === "trigger") {
+            return;
+        }
+    }
+});
 // extracts the certificate chain and sends it to the popup.js
 async function sendRootCAName(details) {
   try {
@@ -11,7 +17,7 @@ async function sendRootCAName(details) {
       { certificateChain: true }
     );
     if (securityInfo.state === "insecure" || securityInfo.state === "broken") {
-      browser.runtime.sendMessage({ message: "no" });
+      // browser.runtime.sendMessage({ message: "no" });
     }
     // get the root certificate authority of the domain
     else if (
@@ -28,9 +34,9 @@ async function sendRootCAName(details) {
       // const root = securityInfo.certificates[securityInfo.certificates.length - 1].issuer; //"subject" property from CertificateInfo Object
       // let rootCA = root.substring(3, root.indexOf(",")); //substring to only include the root CA name (comma seperated list)
 
-      document.addEventListener("DOMContentLoaded", () => {
+      // document.addEventListener("DOMContentLoaded", () => {
         browser.runtime.sendMessage({ rootCA }); //send to popup.js
-      });
+      // });
     }
   } catch (error) {
     console.error(error);
