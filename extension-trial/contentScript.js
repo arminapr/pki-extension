1c372c34703be42155c948d2effa1d9a3b0559bd
akhargha <kharghariaanupam07@gmail.com>
2023-07-05 01:16:39 -0400

    Incomplete block implementation

--- extension-trial/contentScript.js
+++ extension-trial/contentScript.js
@@ -1,36 +1,16 @@
-// Listen for messages
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "blockWebsite") {
-        blockWebsite();
-    }
-});
-
-function blockWebsite() {
-    // Create overlay
-    let overlay = document.createElement('div');
-    overlay.style.position = 'fixed';
-    overlay.style.top = '0';
-    overlay.style.left = '0';
-    overlay.style.width = '100%';
-    overlay.style.height = '100%';
-    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-    overlay.style.zIndex = '9999';
-
-    // Create button
-    let button = document.createElement('button');
-    button.innerText = 'WEBSITE BLOCKED. CLICK ON EXTENSION';
-    button.style.position = 'absolute';
-    button.style.top = '50%';
-    button.style.left = '50%';
-    button.style.transform = 'translate(-50%, -50%)';
-    button.style.fontSize = '20px';
-    button.style.padding = '10px 20px';
-    button.style.backgroundColor = 'blue';
-    button.style.color = 'white';
-
-    // Append button to overlay
-    overlay.appendChild(button);
-
-    // Append overlay to body
-    document.body.appendChild(overlay);
-}
+document.body.innerHTML = `
+  <div style="
+    background-color: red; 
+    width: 100vw; 
+    height: 100vh; 
+    position: fixed; 
+    top: 0; 
+    left: 0; 
+    z-index: 999999; 
+    display: flex; 
+    align-items: center; 
+    justify-content: center; 
+    color: white; 
+    text-align: center;">
+    <h1>This site has been blocked by the Website Blocker extension</h1>
+  </div>`;
